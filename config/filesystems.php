e67747cb91cf74db88bd2ef3c284d8c98e829d7f
jcobhams <tech.supreme1@gmail.com>
2017-07-28 13:36:04 +0100

    wishlist and contact us

--- config/filesystems.php
+++ config/filesystems.php
@@ -58,7 +58,7 @@ return [
         'test' => [
             'driver' => 'local',
             'root' => public_path('uploads'),
-            'url' => env('APP_URL', 'http://127.0.0.1:8000/').'uploads',
+            'url' => env('APP_URL', 'http://127.0.0.1:8000/').'/uploads',
             'visibility' => 'public',
             'max-age' => 14515200
         ],

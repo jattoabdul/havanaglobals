6b876378a0a939c90cce06a37746de740156eedd
jcobhams <tech.supreme1@gmail.com>
2017-07-28 14:02:50 +0100

    wishlist and contact us

--- resources/views/front/index.blade.php
+++ resources/views/front/index.blade.php
@@ -41,7 +41,7 @@
                         <div id="naturix-tab-0" class="tab-pane fade active in">
                             <div class="naturix-slider-1 dots-1">
                                 @foreach($products as $product)
-                                    {{ dd($product->images[0]) }}
+                                    {{ dd($product->images[0]->url) }}
                                     <div class="item">
                                         <div class="product-box">
                                             <div class="product-media">

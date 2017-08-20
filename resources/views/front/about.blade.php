77c71b6cd73033e0b772b603ffe3fc160c338933
jattoabdul <jattoade@gmail.com>
2017-08-20 23:20:46 +0100

    added about page content and replaced lorem ipsum

--- resources/views/front/about.blade.php
+++ resources/views/front/about.blade.php
@@ -37,8 +37,8 @@
     <div class="container rel-div">
         <div class="row">
             <div class="col-sm-6 text-right fsz-16 pt-15 xs-text">
-                <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt
-                    ut laoreet dolore magna aliquam enim ad minim veniam, quis nostrud exerci tation.</p>
+                <p>Everything Organic!!! This is our motto and we are experts in delivering the best 100% organic foods in the market. 
+                    We are able to deliver to a number of places all over the country and always fresh products.</p>
             </div>
             <div class="col-sm-6 text-left xs-text">
                 <h2 class="section-title"> <span class="light-font">we are </span> <strong>Havana farmfood <img src="front/img/icons/logo-icon.png" alt="" /> </strong>                                </h2>
@@ -49,32 +49,32 @@
             <div class="col-md-3 col-sm-6 text-center">
                 <div class="feature-wrap">
                     <img src="front/img/extra/feature-1.png" alt="" />
-                    <h3 class="title-1 ptb-15"> <span class="light-font">fresh from </span> <strong> havana farm</strong> </h3>
-                    <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy. </p>
+                    <h3 class="title-1 ptb-15"> <span class="light-font">ALL from </span> <strong> havana farm</strong> </h3>
+                    <p>Yes, we have it all in one place – Diary products, Meat, Bread, Drinks, Eggs and more!. </p>
                     <a href="#" class="sm-bnt-wht">Read More</a>
                 </div>
             </div>
             <div class="col-md-3 col-sm-6 text-center">
                 <div class="feature-wrap">
                     <img src="front/img/extra/feature-2.png" alt="" />
-                    <h3 class="title-1 ptb-15"> <span class="light-font"> 100%</span> <strong> havana goods</strong> </h3>
-                    <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy. </p>
+                    <h3 class="title-1 ptb-15"> <span class="light-font"> 100%</span> <strong> Organic Goods</strong> </h3>
+                    <p>We are distributing a big variety of products such as Diary, Meat, Bread, Drinks and more. </p>
                     <a href="#" class="sm-bnt-wht">Read More</a>
                 </div>
             </div>
             <div class="col-md-3 col-sm-6 text-center">
                 <div class="feature-wrap">
                     <img src="front/img/extra/feature-3.png" alt="" />
-                    <h3 class="title-1 ptb-15"> <span class="light-font">premium </span> <strong> quality</strong> </h3>
-                    <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy. </p>
+                    <h3 class="title-1 ptb-15"> <span class="light-font">Fast </span> <strong> Delivery</strong> </h3>
+                    <p>We have developed an effective and fast delivery network which brings the products to your home or office up to 48 hours.</p>
                     <a href="#" class="sm-bnt-wht">Read More</a>
                 </div>
             </div>
             <div class="col-md-3 col-sm-6 text-center">
                 <div class="feature-wrap">
                     <img src="front/img/extra/feature-4.png" alt="" />
-                    <h3 class="title-1 ptb-15"> <span class="light-font">100% </span> <strong>natural</strong> </h3>
-                    <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy. </p>
+                    <h3 class="title-1 ptb-15"> <span class="light-font">100% </span> <strong>Difference</strong> </h3>
+                    <p>Our consultants will help you to choose the best plan according to your needs. Note that we can also arrange a custom plan specially for you. </p>
                     <a href="#" class="sm-bnt-wht">Read More</a>
                 </div>
             </div>
@@ -101,11 +101,12 @@
                 <span class="light-font"> WE DO <span class="over-txt light-bg">100% HAVANA GUARANTEE</span>                            </span>
                 <strong>HEALTHY</strong>
             </h2>
-            <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt
-                ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci
-                tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat. Lorem ipsum dolor
-                sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore
-                magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud ullamcorper . </p>
+            <p>Havana is founded by a team of enthusiastic and passionate entrepreneurs. 
+               Havana is the leading supply chain technology platform in agriculture domain in Nigeria.
+               BigHaat has a unique customer focused strategy leveraging the current technology ecosystem
+                that differentiates us from the competitors and thereby providing efficient agro-allied services
+                 to farm produce retailers and consumers.
+             </p>
         </div>
 
         <div class="about-wrap">
@@ -136,7 +137,7 @@
                             <img src="front/img/extra/organic-1.png" alt="" />
                             <span class="divider"></span>
                             <h3 class="title-1"> <a href="#"> <span class="light-font">organic </span> <strong> fruits</strong> </a>                                            </h3>
-                            <i class="clr-txt font-2"> 52 items </i>
+                            <i class="clr-txt font-2"> 52 baskets </i>
                         </div>
                     </div>
                     <div class="col-sm-4">
@@ -151,8 +152,8 @@
                         <div class="organic-prod">
                             <img src="front/img/extra/organic-3.png" alt="" />
                             <span class="divider"></span>
-                            <h3 class="title-1"> <a href="#"> <span class="light-font">organic </span> <strong> breads</strong> </a>                                            </h3>
-                            <i class="clr-txt font-2"> 23 items </i>
+                            <h3 class="title-1"> <a href="#"> <span class="light-font">Fresh </span> <strong> Oil</strong> </a>                                            </h3>
+                            <i class="clr-txt font-2"> 23 Litres </i>
                         </div>
                     </div>
                 </div>
@@ -160,10 +161,10 @@
             <div class="col-lg-6 col-sm-12">
                 <div class="organic-prod-info">
                     <h4 class="sub-title"> FRESH FROM OUR FARM </h4>
-                    <h2 class="section-title ptb-15"> <span class="light-font">220+ </span> <strong>fruits, vegetables </strong> <span class="light-font"> & </span>                                    <strong> lot more</strong> </h2>
-                    <p class="fsz-16">Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt
-                        ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud
-                        exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat.
+                    <h2 class="section-title ptb-15"> <span class="light-font">20+ </span> <strong>fruits, vegetables </strong> <span class="light-font"> & </span><strong> lot more</strong> </h2>
+                    <p class="fsz-16">We have specialized in delivery of organic food/products, produced by our local farms all around the country.
+                    We are distributing a big variety of products such as Diary, Meat, livestocks, Vegetables, Oil and Spices and more. 
+                    Yes we have it all in one place - HAVANA GLOBALS FARM
                     </p>
                 </div>
             </div>
@@ -171,11 +172,12 @@
         <div class="row sec-space-top">
             <div class="col-lg-6 col-sm-12">
                 <div class="organic-prod-info">
-                    <h4 class="sub-title"> FRESH FROM OUR FARM </h4>
-                    <h2 class="section-title ptb-15"> <span class="light-font">115+ </span> <strong>organic juices </strong> <span class="light-font"> and </span>                                    <strong> organic tea</strong> </h2>
-                    <p class="fsz-16">Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt
-                        ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud
-                        exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat.
+                    <h4 class="sub-title"> FAST DELIVERY & SHOPPING EXPERIENCE </h4>
+                    <h2 class="section-title ptb-15"> <span class="light-font">10+ </span> <strong>coverage areas </strong> <span class="light-font"> we </span> <strong> deliver to</strong> </h2>
+                    <p class="fsz-16">How we are different?
+                    Our Consultants, Managers & Farmers will help you to choose the best plan according to your needs.
+                    We deliver to Lagos, Oyo, Kwara(Ilorin), Osun, Ondo, Ogun and Ekiti. For Delivery to Abuja, please contact customer care.
+                    Note that we can also arrange a custom plan specially for you. Give us a call!!! We listen.
                     </p>
                 </div>
             </div>
@@ -185,15 +187,15 @@
                         <div class="organic-prod">
                             <img src="front/img/extra/organic-4.png" alt="" />
                             <span class="divider"></span>
-                            <h3 class="title-1"> <a href="#"> <span class="light-font">organic </span> <strong> juices</strong> </a>                                            </h3>
-                            <i class="clr-txt font-2"> 26 items </i>
+                            <h3 class="title-1"> <a href="#"> <span class="light-font">Healthy </span> <strong> Livestock</strong> </a>                                            </h3>
+                            <i class="clr-txt font-2"> 230 items </i>
                         </div>
                     </div>
                     <div class="col-sm-4">
                         <div class="organic-prod">
                             <img src="front/img/extra/organic-5.png" alt="" />
                             <span class="divider"></span>
-                            <h3 class="title-1"> <a href="#"> <span class="light-font">dried </span> <strong> juices</strong> </a>                                            </h3>
+                            <h3 class="title-1"> <a href="#"> <span class="light-font">Organic </span> <strong> juices</strong> </a>                                            </h3>
                             <i class="clr-txt font-2"> 31 items </i>
                         </div>
                     </div>
@@ -201,7 +203,7 @@
                         <div class="organic-prod">
                             <img src="front/img/extra/organic-6.png" alt="" />
                             <span class="divider"></span>
-                            <h3 class="title-1"> <a href="#"> <span class="light-font">organic </span> <strong> tea</strong> </a>                                            </h3>
+                            <h3 class="title-1"> <a href="#"> <span class="light-font">Grains </span> & <strong> Tubers</strong> </a>                                            </h3>
                             <i class="clr-txt font-2"> 17 items </i>
                         </div>
                     </div>
